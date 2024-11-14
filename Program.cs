f675306df8fb2d92f55534a96cbf09801e5a42a6
Werner Rall <weral@microsoft.com>
2024-11-14 17:33:21 +0200

    fixed certificate type auth and tested

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Files.Shares;
 using AzureStorageManager.Services;
 using System.Security.Cryptography.X509Certificates;
-using System.Security.Cryptography;
 using System.IO;
 
 namespace AzureStorageManager
@@ -15,13 +14,30 @@ namespace AzureStorageManager
             DisplayIntroduction();
 
             // Hardcoded Azure AD credentials
-            string tenantId = "<your-tenant-id>";
-            string clientId = "<your-client-id>";
-            string certificatePath = "<path-to-your-pfx-file>";
-            string certificatePassword = "<pfx-password>";
+            string tenantId = "#TODO";
+            string clientId = "#TODO";
+            string thumbprint = "#TODO";
 
-            // Create the ClientCertificateCredential using X509CertificateLoader
-            var certificate = X509Certificate2.CreateFromEncryptedPemFile(certificatePath, certificatePassword);
+            // Load the certificate from the Windows Certificate Store
+            X509Certificate2 certificate = null;
+            using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, validOnly: false);
+
+                if (certCollection.Count > 0)
+                {
+                    certificate = certCollection[0];
+                }
+                else
+                {
+                    Console.WriteLine("Certificate not found.");
+                    return;
+                }
+
+                store.Close();
+            }
+
             var clientCertificateCredential = new ClientCertificateCredential(tenantId, clientId, certificate);
 
             // Prompt user for storage account details
